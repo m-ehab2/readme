1b51705c908b7812b4ff3df3799bc1751e39d149
m-ehab2 <muhamedehab1@gmail.com>
2023-12-29 03:57:33 +0200

    Completed the slider and made the navbar sticky

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log($("h1"));
+// $(".main-carasoul-main-body").click(function (e) {
+// 	let $carousel = $(this);
+// 	let $cards = $carousel.children(".card");
+// 	let cardWidth = $cards.eq(0).outerWidth(true); // Include margins
+// 	let translateX = $carousel.data("translateX") || 0; // Initial translation or 0
+// 	translateX -= cardWidth;
+// 	$carousel.css({
+// 		transform: `translateX(${translateX}px)`,
+// 		transition: "transform 0.5s ease",
+// 	});
+// 	$carousel.data("translateX", translateX);
+// });
+var slider = tns({
+	items: 3,
+	controls: true,
+	responsive: {
+		350: {
+			items: 1,
+		},
+		750: {
+			items: 2,
+		},
+		1001: {
+			items: 3,
+		},
+	},
+
+	container: "#responsive",
+	swipeAngle: false,
+	speed: 200,
+	nav: false,
+	controlsContainer: ".controlsContainer",
+	mouseDrag: true,
+	gutter: 5,
+});
